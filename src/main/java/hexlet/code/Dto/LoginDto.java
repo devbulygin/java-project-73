9423bd8fb2976b779c0a85ee2ccfc0b9da08cf9a
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-13 02:45:13 +0400

    fix logindto

--- src/main/java/hexlet/code/Dto/LoginDto.java
+++ src/main/java/hexlet/code/Dto/LoginDto.java
@@ -9,5 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginDto {
     private String email;
+
+    private String firstName;
+
+    private String lastName;
     private String password;
 }
