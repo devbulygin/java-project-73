ac263945b4c76505dce5d8f0166bd9010f4f34eb
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-13 23:29:49 +0400

    task dto bug fix

--- src/main/java/hexlet/code/Dto/TaskDto.java
+++ src/main/java/hexlet/code/Dto/TaskDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -23,7 +22,7 @@ public class TaskDto {
 
     private Long taskStatusId;
 
-ё
+
     private Long authorId;
 
     private Long executorId;
