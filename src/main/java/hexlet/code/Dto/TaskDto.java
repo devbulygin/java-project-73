ddb1c56edadc82873012be5f690dfd98df913327
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-16 15:17:05 +0400

    fix

--- src/main/java/hexlet/code/Dto/TaskDto.java
+++ src/main/java/hexlet/code/Dto/TaskDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
@@ -18,7 +19,7 @@ public class TaskDto {
 
     private String description;
 
-    @NotBlank
+    @NotNull
     private Long taskStatusId;
 
     private Set<Long> labelIds;
