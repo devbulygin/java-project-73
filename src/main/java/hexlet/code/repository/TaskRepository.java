6e6e2959ce28ef6daa3ad784b743e6cbcfce6ab1
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-16 22:11:14 +0400

    fix task repository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,11 +1,9 @@
 package hexlet.code.repository;
 
-import hexlet.code.model.QTask;
+
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 @Repository
