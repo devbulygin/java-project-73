2282289539164363340fb6a7b56df823e805625e
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-16 17:27:07 +0400

    fix

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,20 +1,17 @@
+
+
+
 package hexlet.code.repository;
 
-import hexlet.code.model.QTask;
 import hexlet.code.model.Task;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TaskRepository extends
-        JpaRepository<Task, Long>,
-        QuerydslPredicateExecutor<Task>,
-        QuerydslBinderCustomizer<QTask> {
-    @Override
-    default void customize(QuerydslBindings bindings, QTask task) {
-    }
+import java.util.Optional;
 
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
+    Optional<Task> findByName(String name);
 }
