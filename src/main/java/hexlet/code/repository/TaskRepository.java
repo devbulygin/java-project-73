4cbfe669ef945bfa97b1e12862c8b041c50c1408
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-19 02:40:45 +0400

    fix taskController

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,32 +1,12 @@
 package hexlet.code.repository;
 
 
-//@Repository
-//public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
-//
-//}
-
-import com.querydsl.core.types.dsl.StringPath;
-import hexlet.code.model.QTask;
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
-import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.dsl.StringExpression;
-
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task>,
-        QuerydslBinderCustomizer<QTask> {
+public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
 
-    @Override
-    default void customize(QuerydslBindings bindings, QTask task) {
-        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>)
-                StringExpression::containsIgnoreCase);
-        bindings.excluding(task.id);
-        bindings.bind(task.labels.any().name).first((path, value) -> path.containsIgnoreCase(value));
-    }
 }
