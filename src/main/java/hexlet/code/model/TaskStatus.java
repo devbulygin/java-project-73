fec60c7f14aa0f4af7272752e6f0bd96f033b88a
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-21 01:56:04 +0400

    fix

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -9,37 +9,33 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
+import static javax.persistence.GenerationType.IDENTITY;
 import static javax.persistence.TemporalType.TIMESTAMP;
 
-@Entity
-@Table(name = "task_statuses")
 @Getter
 @Setter
+@Entity
+@Table(name = "statuses")
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskStatus {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @NotBlank
-    @Size(min = 1)
-    @Column(unique = true)
+    @Column(columnDefinition = "varchar(1001)")
     private String name;
 
     @CreationTimestamp
     @Temporal(TIMESTAMP)
     private Date createdAt;
 
-    public TaskStatus(Long id) {
-        this.id = id;
-    }
 }
