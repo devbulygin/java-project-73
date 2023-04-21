4c165c212183133e8adf05dcc39a8621ca15009e
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-21 22:44:25 +0400

    list users on main page

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,13 +1,27 @@
 package hexlet.code.controller;
 
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public final class WelcomeController {
 
+    private UserRepository userRepository;
+    @Operation(summary = "Get all users")
+    @ApiResponses(@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = User.class))))
     @GetMapping(path = "/")
-    public String root() {
-        return "Welcome to Spring";
+    public List<User> getAll() {
+        return userRepository.findAll()
+                .stream()
+                .toList();
     }
 }
