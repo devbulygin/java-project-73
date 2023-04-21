ad6d31cdfcdf99c607abf1ffcd9cb72c6fd9dae5
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-04-21 23:09:21 +0400

    fix welcome controller

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,27 +1,15 @@
 package hexlet.code.controller;
 
-import hexlet.code.model.User;
-import hexlet.code.repository.UserRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public final class WelcomeController {
 
-    private UserRepository userRepository;
-    @Operation(summary = "Get all users")
-    @ApiResponses(@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = User.class))))
+
     @GetMapping(path = "/")
-    public List<User> getAll() {
-        return userRepository.findAll()
-                .stream()
-                .toList();
+    public RedirectView root() {
+        return new RedirectView("/api/users");
     }
 }
